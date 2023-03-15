1e798b2b53e49aaad01a6c9a01e1f5df55bc182f
Chris Mellor <cmellor@gmail.com>
2023-03-15 23:26:18 +0000

    feat: add migration

---
+++ database/migrations/create_blocks_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create(config('blockade.blocks_table', 'blocks'), function (Blueprint $table) {
+            $table->foreignId(config('blockade.blocker_foreign_key', 'blocker_id'))
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->foreignId(config('blockade.blocked_foreign_key', 'blocked_id'))
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->timestamp('expires_at')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists(config(key: 'blockade.blocks_table', default: 'blocks'));
+    }
+};
